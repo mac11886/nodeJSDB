dc31afe8a1298d5af1aaaedbade57fcaf578ce38
bell <weerin.n@ku.th>
2021-07-05 11:13:11 +0700

    .env & edit too many connection bug

--- sqlapp/app.js
+++ sqlapp/app.js
@@ -16,8 +16,8 @@ var app = express();
 createPool()
 
 // view engine setup
-// app.set("views", path.join(__dirname, "views"));
-// app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 // app.set("view engine", "ejs");
 
 app.use(logger("dev"));
